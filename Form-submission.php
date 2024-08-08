e25f4923587eed9e62d1f16d0cc6a9e585f7fc9e
Imelda Malcellari <144271408+imelda-m@users.noreply.github.com>
2024-08-08 12:58:35 +0200

    Update Form-submission.php

--- Form-submission.php
+++ Form-submission.php
@@ -1,25 +1,20 @@
 <?php
-    // Including DatabaseActivity class.
-    require('database.php');
+require('database.php');
+require('ProductFactory.php');
+require('ProductManager.php');
 
-    // Initialize DatabaseActivity class for further operations.
-    $db = new Database();
+$db = new Database();
+$productManager = new ProductManager($db);
 
-    // Checks once form is submitted (necessary to call needed function based on the input of the form).
-    if(!isset($_POST['submit']) && isset($_POST['nameInput'])) // If form contains input for the name - form adds product to database.
-        $db->addProductToDb( $_POST['skuInput'],
-        $_POST['nameInput'],
-        $_POST['price'],
-        $_POST['productType'],
-        $_POST['size'] ?? null,
-        $_POST['weight'] ?? null,
-        $_POST['height'] ?? null,
-        $_POST['width'] ?? null,
-        $_POST['length'] ?? null);
-    else if(!isset($_POST['submit']) && !isset($_POST['nameInput'])) // Otherwise - form deletes checked products from database.
-        $db->deleteCheckedProductsFromDatabase(); 
-        
-       
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['nameInput'])) {
+        $product = ProductFactory::createProduct($_POST);
+        $productManager->addProductToDb($product);
+    } else {
+        $productManager->deleteCheckedProducts();
+    }
+    
+    header("Location: ProductList.php");
+    exit();
+}
 ?>
-
-<
